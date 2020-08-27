14a0b718d57fd17b5031ba2da0a3ed459396c957
amandadallag <amandadalag@gmail.com>
2020-08-26 22:59:14 -0300

    modificada página inicial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,25 +3,18 @@
 @section('content')
 
 
-    <div class="jumbotron text-center"  style="background-color: #b0edf5;">
+<section> 
         <div class="row">
-            <div class="col-sm-4" >
-            <img src="https://i.ibb.co/R0PLhwK/cachorro-menu.png" alt="cachorro-menu" height="350" width="360">
+            <div class="col-md-5">
+                <img src="https://i.ibb.co/R0PLhwK/cachorro-menu.png" alt="avatar image" class="imagem-principal" height="500">
+                <!-- <img src="https://i.ibb.co/R0PLhwK/cachorro-menu.png" alt="cachorro-menu" height="350" width="360"> -->
             </div>
-            <div class="col-sm-8">
-                <div class="container-fluid">
-                    <h1>For All</h1>
-                    <p>
-                        O For All é um site 100% gratuito e seguro que tem como objetivo levar o acesso à leitura para crianças 
-                        com deficiência visual e auditiva. O site funciona por meio de audiobooks e videobooks compartilhados 
-                        pelos próprios usuários.
-                    </p>
-                    <p>
-                        Venha você também contribuir e aproveitar!
-                    </p>
-                </div>
+            <div class="col-md-6">
+            <h1 class="text-white titulo-principal">FOR ALL</h1>
+            <p class="texto-principal text-white">O For All é um site 100% gratuito e seguro, que tem como 
+                objetivo levar o acesso a leitura para crianças com deficiência visual e auditiva. Através de audiobooks 
+                e videobooks compartilhados pelos próprios usuários, muitos serão os beneficiados</p>
             </div>
         </div>
-
-    </div>
+    </section>
 @endsection
